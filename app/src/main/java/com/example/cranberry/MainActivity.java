2c9aeb7de88049a5d6b6ab32df6fb0d151cba0c1
CompiledError <70342656+CompiledError@users.noreply.github.com>
2023-09-04 15:49:12 -0700

    Buttons for CS 301 practice

--- app/src/main/java/com/example/cranberry/MainActivity.java
+++ app/src/main/java/com/example/cranberry/MainActivity.java
@@ -3,16 +3,14 @@ package com.example.cranberry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
+
 }
